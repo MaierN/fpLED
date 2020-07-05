b39e42e363731f4783d73c8a9357c9324ec19301
Nicolas Maier <Nicolas.Maier@edu.hefr.ch>
2020-07-05 19:56:19 +0200

    removed unused code

--- tests_technologiques/platformio-stm32-test/src/exception_handler/exception_handler.h
+++
@@ -1,7 +0,0 @@
-
-#ifndef IT_HANDLER_H
-#define IT_HANDLER_H
-
-void exception_handler_default_error();
-
-#endif
--- tests_technologiques/platformio-stm32-test/src/leds/leds.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef LEDS_H
-#define LEDS_H
-
-void leds_init();
-void leds_set_pixel(size_t strip, size_t pixel, uint8_t r, uint8_t g, uint8_t b);
-
-#endif
