f2b70717151c866190a1b593e25b90ce2ad73bec
Nicolas Maier <Nicolas.Maier@edu.hefr.ch>
2020-07-14 05:49:19 +0200

    fix

--- realisation/host/c/demo_telecom_tower.c
+++ realisation/host/c/demo_telecom_tower.c
@@ -15,10 +15,10 @@ int main(int argc, char** argv) {
 
     led_controller_t lc = {
         argv[1], // char* device_path
-        8,       // size_t channel_n
+        5,       // size_t channel_n
         256,     // size_t led_n
         true,    // bool do_gamma_correction
-        0,       // led_controller_channel_t* channels
+        NULL,       // led_controller_channel_t* channels
     };
     led_controller_init(&lc);
 
